55daa7cc0be45ce76434ae7059efbb3745ed1bfa
tcjardine <112444700+tcjardine@users.noreply.github.com>
2023-07-12 11:48:06 -0700

    Integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleReponse(response) {
+    console.log(response.date[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleReponse);
   }
 
   function handleKeywordChange(event) {
