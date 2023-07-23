533d8e723012ee5f929fb1973be48674946f18fb
tcjardine <112444700+tcjardine@users.noreply.github.com>
2023-07-23 14:22:53 -0700

    Integrated photos section + css properties

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
-
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "DUracYpMRbqTuTLDn6oQMD848tFOuFdKkkdRQPfJ3eZbBMfm8YqpXigv";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: pexelsApiKey };
+    axios.get(pexelsApiUrl, { headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -25,9 +36,7 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <h1>
-        What word do you want to look up?
-      </h1>
+      <h1>What word do you want to look up?</h1>
       <section>
         <form onSubmit={search}>
           <input
@@ -41,6 +50,7 @@ export default function Dictionary() {
         </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
